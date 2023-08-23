5f0504cb40581b29156a9a24d419a3586d9c7a5d
Filler <gggaaappo@gmail.com>
2023-08-23 01:50:26 -0500

    Initial commit

---
+++ src/main/java/net/filler/spitemod/SpiteModClient.java
@@ -0,0 +1,10 @@
+package net.filler.spitemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpiteModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
